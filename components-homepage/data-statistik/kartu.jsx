d9fc1f218b8b1602d4a36cae2baf470e458eeeb4
RaihanAnnaji <raihanannaji1990@gmail.com>
2025-08-15 09:29:41 +0700

    Update and rename kartu-sts.jsx to kartu.jsx

--- components-homepage/data-statistik/kartu.jsx
+++ components-homepage/data-statistik/kartu.jsx
@@ -4,9 +4,8 @@ import Image from 'next/image'
 
 const Kartu = ({ title, description, jumlahData, imageSrc, onClick }) => {
   return (
-    <div
-      className="group relative bg-white rounded-2xl p-6 w-full mx-auto text-center shadow-sm border border-slate-800/30 transition-all duration-300 transform hover:-translate-y-1"
-    >
+    <div className="group relative bg-white rounded-2xl p-6 w-full mx-auto text-center shadow-sm border border-slate-800/30 transition-all duration-300 transform hover:-translate-y-1">
+
       {/* Gambar */}
       <div className="w-40 h-40 mx-auto mb-4 relative transition-transform duration-300 ease-in-out group-hover:scale-110">
         <Image
@@ -17,14 +16,14 @@ const Kartu = ({ title, description, jumlahData, imageSrc, onClick }) => {
         />
       </div>
 
-      {/* Judul & deskripsi */}
+      {/* Judul dan deskripsi */}
       <h3 className="text-lg font-semibold text-gray-800 mb-1">{title}</h3>
       <p className="text-sm text-gray-600">{description}</p>
 
-      {/* Tombol yang bisa diklik */}
+      {/* Tombol */}
       <button
         onClick={onClick}
-        className="mt-4 bg-gradient-to-r from-[#01BBA6] to-[#007686] text-white px-4 py-2 rounded-full text-sm transition duration-300 hover:from-[#007686] hover:to-[#007686]"
+        className="mt-4 bg-gradient-to-r from-[#01BBA6] to-[#007686] text-white px-4 py-2 rounded-full text-sm transition duration-300 hover:from-[#007686] hover:to-[#007686] cursor-pointer"
       >
         Lihat Selengkapnya
       </button>
